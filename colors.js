c7f5bacd4c31962c37e12985a70a58918c83960b
Leah Tynan <brunetto@alum.mit.edu>
2020-03-14 21:30:53 -0400

    Removed Masonry, evened out sizing of different shapes

--- colors.js
+++ colors.js
@@ -11,14 +11,14 @@ var data = {
 		"hex": "ffff00",
 		"files": ["leaf_yellow", "star", "rubber-ducky_yellow", "rubber-ducky_yellow", "nail-polish_yellow", "star", "leaf_yellow", "star"],
 		"description": ["leaf", "star", "rubber ducky", "rubber ducky", "nail polish", "star", "leaf", "star"],
-		"shapes": ["vertical", "square", "horizontal", "horizontal", "vertical", "square", "leaf_yellow", "square"]
+		"shapes": ["vertical", "2x", "horizontal", "horizontal", "vertical", "square", "leaf_yellow", "square"]
 	},
 	"Green": {
 		"angle": -90,
 		"hex": "63bb45",
 		"files": ["bow_green", "measuring-cup", "rubber-ducky_green", "hedgehog_green", "nail-polish_green", "measuring-cup", "snake-plant", "succulent"],
 		"description": ["bow", "measuring cup", "rubber ducky", "hedgehog", "nail polish", "measuring cup", "snake plant", "succuluent"],
-		"shapes": ["square", "horizontal", "horizontal", "horizontal", "vertical", "horizontal", "square", "square"]
+		"shapes": ["square", "horizontal", "horizontal", "horizontal", "2x", "horizontal", "square", "square"]
 	},
 	"Blue": {
 		"angle": -150,
@@ -32,7 +32,7 @@ var data = {
 		"hex": "6f22b6",
 		"files": ["amethyst-shard", "dumbbell", "glass-jar_purple", "amethyst-chunk", "nail-polish_purple", "coil", "amethyst-chunk", "dumbbell"],
 		"description": ["amethyst shard", "dumbbell", "cylindrical glass jar", "amethyst chunk", "nail polish", "coil of fabric", "amethyst chunk", "dumbbell"],
-		"shapes": ["vertical", "horizontal", "vertical", "horizontal", "vertical", "square", "horizontal", "horizontal"]
+		"shapes": ["vertical", "horizontal", "vertical", "horizontal", "vertical", "2x", "horizontal", "horizontal"]
 	},
 	"Red": {
 		"angle": 90,
@@ -46,7 +46,7 @@ var data = {
 		"hex": "ffa500",
 		"files": ["leaf_orange", "citrine", "cat-dish", "leaf_orange", "nail-polish_orange", "leaf_orange", "leaf_orange", "citrine"],
 		"description": ["leaf", "citrine", "cat-shaped dish", "leaf", "nail polish", "leaf", "leaf", "citrine"],
-		"shapes": ["square", "square", "horizontal", "square", "vertical", "square", "square", "square"]
+		"shapes": ["square", "2x", "horizontal", "square", "vertical", "square", "square", "square"]
 	},
 }
 
@@ -73,34 +73,25 @@ function changeColor(direction) {
 	document.getElementById("color-wheel").style = "transform: rotate(" + data[color].angle + "deg)";
 	document.getElementById("title").innerHTML = colors[position];
 	var images = document.getElementsByClassName('image-to-update'); // The images
-	var gridItems = document.getElementsByClassName('grid-item'); // The frames that hold the images
-	console.log("Number of grid items: " + gridItems.length);
+	var items = document.getElementsByClassName('item'); // The frames that hold the images
 	for (i = 0; i < numberItems; i++) { 
 		// TODO: Need to load this data for Yellow when the page is opened
-		console.log("Filename: " + data[color].files[i]);
 		images[i].src = "images/" + data[color].files[i] + ".png";
 		images[i].alt = color + " " + data[color].description[i];
 		images[i].title = color + " " + data[color].description[i];
 		var shape = data[color].shapes[i];
-		console.log("Shape: " + shape)
-		clearPreviousShape(gridItems[i]);
+		clearPreviousShape(items[i]);
 		// If the item's shape is not square, add additional classes to set those propo
 		if (shape == "vertical" || shape == "horizontal") {
-			console.log("++++ Setting the shape to: " + shape);
-			gridItems[i].classList.add(shape);
+			items[i].classList.add(shape);
 		}
     }
 }
 
-function clearPreviousShape(gridItem) {
-	if (gridItem.classList.length > 1) {
-		console.log("Should remove: " + gridItem.classList[1]);
-		var classToRemove = gridItem.classList[1];
-		gridItem.classList.remove(classToRemove);
-		console.log("---> removed " + classToRemove);
-		console.log("New size of class list: " + gridItem.classList.length);
-	} else {
-		console.log("Don't need to remove a shape class");
+function clearPreviousShape(item) {
+	if (item.classList.length > 1) {
+		var classToRemove = item.classList[1];
+		item.classList.remove(classToRemove);
 	}
 }
 
